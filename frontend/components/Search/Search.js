6abca13846465eca03b81be7c4471f8858a31b2b
Brandon Pereira <brandonpereira@Brandons-Mac-mini.local>
2019-12-20 22:19:28 -0700

    hook up clicks in search

--- frontend/components/Search/Search.js
+++ frontend/components/Search/Search.js
@@ -10,13 +10,15 @@ function Search() {
   const [value, setValue] = useState("");
   const { loading, error, data } = useSearchPredictions(value);
 
+
+
   const isPredicting = Boolean(value);
 
   const onClose = useCallback(() => {
     setValue("");
   }, []);
 
-  const onSelect = useCallback(id => {
+  const onSelect = useCallback((id) => () => {
     console.log(id);
   }, []);
   console.log(data);
@@ -31,7 +33,7 @@ function Search() {
       <Predictions isPredicting={isPredicting}>
         {loading && <Loader color="#fff" />}
         {!loading &&
-          data.map(result => <SearchPrediction key={result.id} {...result} />)}
+          data.map(result => <SearchPrediction key={result.id} {...result} onSelect={onSelect(result.id)} />)}
       </Predictions>
     </Container>
   );
